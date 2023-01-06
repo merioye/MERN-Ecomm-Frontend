54ef9b79c8115374364e42f5cb1f37172c7689b9
Umair Saleem <saleemakhtar35302@gmail.com>
2023-01-06 20:34:04 +0500

    resolved errors that was occurrring during build process

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import {
-    useTheme,
-    Box,
-    Typography,
-    Button,
-    Divider,
-    Avatar,
-    CircularProgress,
+  useTheme,
+  Box,
+  Typography,
+  Button,
+  Divider,
+  Avatar,
+  CircularProgress,
 } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -17,182 +17,182 @@ import AuthInputBox from "components/shared/AuthInputBox";
 import CustomAlert from "components/shared/CustomAlert";
 
 const Login = () => {
-    const { alert } = useSelector((state) => state.alert);
-    const { user } = useSelector((state) => state.auth);
-    const [values, setValues] = useState({ email: "", password: "" });
-    const [showLoginLoader, setShowLoginLoader] = useState(false);
-    const dispatch = useDispatch();
-    const theme = useTheme();
-    const router = useRouter();
-    const { error } = router.query;
+  const { alert } = useSelector((state) => state.alert);
+  const { user } = useSelector((state) => state.auth);
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [showLoginLoader, setShowLoginLoader] = useState(false);
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const router = useRouter();
+  const { error } = router.query;
 
-    useEffect(() => {
-        if (error) {
-            dispatch(showErrorAlert(error));
-            router.replace("/login", undefined, { shallow: true });
-            document.title = "Login";
-        }
-    }, [error, router, dispatch]);
+  useEffect(() => {
+    if (error) {
+      dispatch(showErrorAlert(error));
+      router.replace("/login", undefined, { shallow: true });
+      document.title = "Login";
+    }
+  }, [error, router, dispatch]);
 
-    useEffect(() => {
-        if (user) {
-            router.push("/");
-        }
-    }, [user, router]);
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
 
-    const handleLogin = () => {
-        if (!values.email.trim().length || !values.password.trim().length) {
-            dispatch(showErrorAlert("All fields are required"));
-            return;
-        }
-        setShowLoginLoader(true);
-        dispatch(login(values, setShowLoginLoader, router));
-    };
+  const handleLogin = () => {
+    if (!values.email.trim().length || !values.password.trim().length) {
+      dispatch(showErrorAlert("All fields are required"));
+      return;
+    }
+    setShowLoginLoader(true);
+    dispatch(login(values, setShowLoginLoader, router));
+  };
 
-    return (
-        <>
-            <Box
-                sx={{
-                    ...theme.authPageContainer,
-                    py: { xs: "0px", md: "50px" },
+  return (
+    <>
+      <Box
+        sx={{
+          ...theme.authPageContainer,
+          py: { xs: "0px", md: "50px" },
+        }}
+      >
+        <Box
+          sx={theme.authFormContainer}
+          style={{ boxShadow: theme.palette.boxShadow.modal }}
+        >
+          <Typography variant="h5" sx={theme.authFormTitle}>
+            Welcome To TeleCart
+          </Typography>
+          <Typography variant="h6" sx={theme.authFormInfo}>
+            Log in with email & password
+          </Typography>
+          <AuthInputBox
+            label="Email"
+            placeholder="example@mail.com"
+            name="email"
+            type="email"
+            values={values}
+            setValues={setValues}
+          />
+          <AuthInputBox
+            label="Password"
+            placeholder="********"
+            type="password"
+            name="password"
+            values={values}
+            setValues={setValues}
+            fromLoginPage={true}
+          />
+          <Button
+            variant="contained"
+            disabled={showLoginLoader}
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "pink.dark",
+              mt: 3,
+              "&:hover": { backgroundColor: "pink.darkHover" },
+            }}
+            onClick={handleLogin}
+          >
+            {showLoginLoader ? (
+              <CircularProgress
+                sx={{ color: theme.palette.pink.dark }}
+                size={25}
+              />
+            ) : (
+              <>Login</>
+            )}
+          </Button>
+          <Divider sx={{ mt: 3, color: "text.light" }}>or</Divider>
+          <Button
+            variant="contained"
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "bg.navy",
+              fontSize: "12px",
+              mt: 3,
+              "&:hover": { backgroundColor: "bg.navy" },
+            }}
+            onClick={() =>
+              window.open(
+                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/facebook`,
+                "_self"
+              )
+            }
+          >
+            <Avatar
+              src="/images/facebook-logo.jpg"
+              alt="logo"
+              sx={theme.authFormSocialLogo}
+            />
+            continue with facebook
+          </Button>
+          <Button
+            variant="contained"
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "bg.blue",
+              fontSize: "12px",
+              mt: 1.5,
+              "&:hover": { backgroundColor: "bg.blue" },
+            }}
+            onClick={() =>
+              window.open(
+                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/google`,
+                "_self"
+              )
+            }
+          >
+            <Avatar
+              src="/images/google-logo.jpg"
+              alt="logo"
+              sx={theme.authFormSocialLogo}
+            />
+            continue with google
+          </Button>
+          <Typography sx={{ ...theme.authFormOtherPageLinkText }}>
+            Don&apos;t have account?
+            <Link href="/register">
+              <a
+                style={{
+                  ...theme.authFormLink,
+                  borderBottom: `1px solid ${theme.palette.text.primary}`,
                 }}
-            >
-                <Box
-                    sx={theme.authFormContainer}
-                    style={{ boxShadow: theme.palette.boxShadow.modal }}
-                >
-                    <Typography variant="h5" sx={theme.authFormTitle}>
-                        Welcome To TeleCart
-                    </Typography>
-                    <Typography variant="h6" sx={theme.authFormInfo}>
-                        Log in with email & password
-                    </Typography>
-                    <AuthInputBox
-                        label="Email"
-                        placeholder="example@mail.com"
-                        name="email"
-                        type="email"
-                        values={values}
-                        setValues={setValues}
-                    />
-                    <AuthInputBox
-                        label="Password"
-                        placeholder="********"
-                        type="password"
-                        name="password"
-                        values={values}
-                        setValues={setValues}
-                        fromLoginPage={true}
-                    />
-                    <Button
-                        variant="contained"
-                        disabled={showLoginLoader}
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "pink.dark",
-                            mt: 3,
-                            "&:hover": { backgroundColor: "pink.darkHover" },
-                        }}
-                        onClick={handleLogin}
-                    >
-                        {showLoginLoader ? (
-                            <CircularProgress
-                                sx={{ color: theme.palette.pink.dark }}
-                                size={25}
-                            />
-                        ) : (
-                            <>Login</>
-                        )}
-                    </Button>
-                    <Divider sx={{ mt: 3, color: "text.light" }}>or</Divider>
-                    <Button
-                        variant="contained"
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "bg.navy",
-                            fontSize: "12px",
-                            mt: 3,
-                            "&:hover": { backgroundColor: "bg.navy" },
-                        }}
-                        onClick={() =>
-                            window.open(
-                                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/facebook`,
-                                "_self"
-                            )
-                        }
-                    >
-                        <Avatar
-                            src="/images/facebook-logo.jpg"
-                            alt="logo"
-                            sx={theme.authFormSocialLogo}
-                        />
-                        continue with facebook
-                    </Button>
-                    <Button
-                        variant="contained"
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "bg.blue",
-                            fontSize: "12px",
-                            mt: 1.5,
-                            "&:hover": { backgroundColor: "bg.blue" },
-                        }}
-                        onClick={() =>
-                            window.open(
-                                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/google`,
-                                "_self"
-                            )
-                        }
-                    >
-                        <Avatar
-                            src="/images/google-logo.jpg"
-                            alt="logo"
-                            sx={theme.authFormSocialLogo}
-                        />
-                        continue with google
-                    </Button>
-                    <Typography sx={{ ...theme.authFormOtherPageLinkText }}>
-                        Don't have account?
-                        <Link href="/register">
-                            <a
-                                style={{
-                                    ...theme.authFormLink,
-                                    borderBottom: `1px solid ${theme.palette.text.primary}`,
-                                }}
-                            >
-                                Sign Up
-                            </a>
-                        </Link>
-                    </Typography>
-                    <Typography
-                        sx={{
-                            ...theme.authFormOtherPageLinkText,
-                            backgroundColor: "bg.aliceBlue",
-                            padding: "20px",
-                            borderRadius: "4px",
-                        }}
-                    >
-                        Forgot your password?
-                        <Link href="/forgotpassword">
-                            <a
-                                style={{
-                                    ...theme.authFormLink,
-                                    borderBottom: `1px solid ${theme.palette.text.primary}`,
-                                }}
-                            >
-                                Reset It
-                            </a>
-                        </Link>
-                    </Typography>
-                </Box>
-            </Box>
-            {alert.showAlert && <CustomAlert />}
-        </>
-    );
+              >
+                Sign Up
+              </a>
+            </Link>
+          </Typography>
+          <Typography
+            sx={{
+              ...theme.authFormOtherPageLinkText,
+              backgroundColor: "bg.aliceBlue",
+              padding: "20px",
+              borderRadius: "4px",
+            }}
+          >
+            Forgot your password?
+            <Link href="/forgotpassword">
+              <a
+                style={{
+                  ...theme.authFormLink,
+                  borderBottom: `1px solid ${theme.palette.text.primary}`,
+                }}
+              >
+                Reset It
+              </a>
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+      {alert.showAlert && <CustomAlert />}
+    </>
+  );
 };
 
 export default Login;
 
 Login.getLayout = (page) => {
-    return <>{page}</>;
+  return <>{page}</>;
 };
--- pages/resetpassword/[resetPasswordToken]/[userId].js
+++ pages/resetpassword/[resetPasswordToken]/[userId].js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import {
-    useTheme,
-    Box,
-    Typography,
-    Button,
-    Divider,
-    Avatar,
-    CircularProgress,
+  useTheme,
+  Box,
+  Typography,
+  Button,
+  Divider,
+  Avatar,
+  CircularProgress,
 } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -19,176 +19,176 @@ import regex from "regexUtils/regex";
 import checkIsValid from "regexUtils/checkIsValid";
 
 const ResetPassword = () => {
-    const { alert } = useSelector((state) => state.alert);
-    const { user } = useSelector((state) => state.auth);
-    const [values, setValues] = useState({ password: "", retypePassword: "" });
-    const [showResetLoader, setShowResetLoader] = useState(false);
-    const dispatch = useDispatch();
-    const theme = useTheme();
-    const router = useRouter();
-    const { query } = router;
+  const { alert } = useSelector((state) => state.alert);
+  const { user } = useSelector((state) => state.auth);
+  const [values, setValues] = useState({ password: "", retypePassword: "" });
+  const [showResetLoader, setShowResetLoader] = useState(false);
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const router = useRouter();
+  const { query } = router;
 
-    useEffect(() => {
-        if (user) {
-            router.push("/");
-        }
-    }, [user, router]);
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
 
-    const handleResetPassword = () => {
-        if (
-            !checkIsValid(values.password, regex.password) ||
-            values.password !== values.retypePassword
-        ) {
-            dispatch(showErrorAlert("Please fill the fields data correctly"));
-            return;
-        }
-        setShowResetLoader(true);
-        dispatch(resetPassword(values, setShowResetLoader, query, router));
-    };
+  const handleResetPassword = () => {
+    if (
+      !checkIsValid(values.password, regex.password) ||
+      values.password !== values.retypePassword
+    ) {
+      dispatch(showErrorAlert("Please fill the fields data correctly"));
+      return;
+    }
+    setShowResetLoader(true);
+    dispatch(resetPassword(values, setShowResetLoader, query, router));
+  };
 
-    return (
-        <>
-            <Box
-                sx={{
-                    ...theme.authPageContainer,
-                    py: { xs: "0px", md: "50px" },
+  return (
+    <>
+      <Box
+        sx={{
+          ...theme.authPageContainer,
+          py: { xs: "0px", md: "50px" },
+        }}
+      >
+        <Box
+          sx={theme.authFormContainer}
+          style={{ boxShadow: theme.palette.boxShadow.modal }}
+        >
+          <Typography variant="h5" sx={theme.authFormTitle}>
+            Reset Your Paassword
+          </Typography>
+          <Typography variant="h6" sx={theme.authFormInfo}>
+            Enter your new password
+          </Typography>
+          <AuthInputBox
+            label="Password"
+            placeholder="********"
+            type="password"
+            name="password"
+            values={values}
+            setValues={setValues}
+          />
+          <AuthInputBox
+            label="Retype Password"
+            placeholder="********"
+            type="password"
+            name="retypePassword"
+            values={values}
+            setValues={setValues}
+          />
+          <Button
+            variant="contained"
+            disabled={showResetLoader}
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "pink.dark",
+              mt: 3,
+              "&:hover": { backgroundColor: "pink.darkHover" },
+            }}
+            onClick={handleResetPassword}
+          >
+            {showResetLoader ? (
+              <CircularProgress
+                sx={{ color: theme.palette.pink.dark }}
+                size={25}
+              />
+            ) : (
+              <>reset password</>
+            )}
+          </Button>
+          <Divider sx={{ mt: 3, color: "text.light" }}>or</Divider>
+          <Button
+            variant="contained"
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "bg.navy",
+              fontSize: "12px",
+              mt: 3,
+              "&:hover": { backgroundColor: "bg.navy" },
+            }}
+            onClick={() =>
+              window.open(
+                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/facebook`,
+                "_self"
+              )
+            }
+          >
+            <Avatar
+              src="/images/facebook-logo.jpg"
+              alt="logo"
+              sx={theme.authFormSocialLogo}
+            />
+            continue with facebook
+          </Button>
+          <Button
+            variant="contained"
+            style={theme.authBtnStyle}
+            sx={{
+              backgroundColor: "bg.blue",
+              fontSize: "12px",
+              mt: 1.5,
+              "&:hover": { backgroundColor: "bg.blue" },
+            }}
+            onClick={() =>
+              window.open(
+                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/google`,
+                "_self"
+              )
+            }
+          >
+            <Avatar
+              src="/images/google-logo.jpg"
+              alt="logo"
+              sx={theme.authFormSocialLogo}
+            />
+            continue with google
+          </Button>
+          <Typography sx={{ ...theme.authFormOtherPageLinkText }}>
+            Remember your password?
+            <Link href="/login">
+              <a
+                style={{
+                  ...theme.authFormLink,
+                  borderBottom: `1px solid ${theme.palette.text.primary}`,
                 }}
-            >
-                <Box
-                    sx={theme.authFormContainer}
-                    style={{ boxShadow: theme.palette.boxShadow.modal }}
-                >
-                    <Typography variant="h5" sx={theme.authFormTitle}>
-                        Reset Your Paassword
-                    </Typography>
-                    <Typography variant="h6" sx={theme.authFormInfo}>
-                        Enter your new password
-                    </Typography>
-                    <AuthInputBox
-                        label="Password"
-                        placeholder="********"
-                        type="password"
-                        name="password"
-                        values={values}
-                        setValues={setValues}
-                    />
-                    <AuthInputBox
-                        label="Retype Password"
-                        placeholder="********"
-                        type="password"
-                        name="retypePassword"
-                        values={values}
-                        setValues={setValues}
-                    />
-                    <Button
-                        variant="contained"
-                        disabled={showResetLoader}
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "pink.dark",
-                            mt: 3,
-                            "&:hover": { backgroundColor: "pink.darkHover" },
-                        }}
-                        onClick={handleResetPassword}
-                    >
-                        {showResetLoader ? (
-                            <CircularProgress
-                                sx={{ color: theme.palette.pink.dark }}
-                                size={25}
-                            />
-                        ) : (
-                            <>reset password</>
-                        )}
-                    </Button>
-                    <Divider sx={{ mt: 3, color: "text.light" }}>or</Divider>
-                    <Button
-                        variant="contained"
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "bg.navy",
-                            fontSize: "12px",
-                            mt: 3,
-                            "&:hover": { backgroundColor: "bg.navy" },
-                        }}
-                        onClick={() =>
-                            window.open(
-                                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/facebook`,
-                                "_self"
-                            )
-                        }
-                    >
-                        <Avatar
-                            src="/images/facebook-logo.jpg"
-                            alt="logo"
-                            sx={theme.authFormSocialLogo}
-                        />
-                        continue with facebook
-                    </Button>
-                    <Button
-                        variant="contained"
-                        style={theme.authBtnStyle}
-                        sx={{
-                            backgroundColor: "bg.blue",
-                            fontSize: "12px",
-                            mt: 1.5,
-                            "&:hover": { backgroundColor: "bg.blue" },
-                        }}
-                        onClick={() =>
-                            window.open(
-                                `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/login/google`,
-                                "_self"
-                            )
-                        }
-                    >
-                        <Avatar
-                            src="/images/google-logo.jpg"
-                            alt="logo"
-                            sx={theme.authFormSocialLogo}
-                        />
-                        continue with google
-                    </Button>
-                    <Typography sx={{ ...theme.authFormOtherPageLinkText }}>
-                        Remember your password?
-                        <Link href="/login">
-                            <a
-                                style={{
-                                    ...theme.authFormLink,
-                                    borderBottom: `1px solid ${theme.palette.text.primary}`,
-                                }}
-                            >
-                                Login Here
-                            </a>
-                        </Link>
-                    </Typography>
-                    <Typography
-                        sx={{
-                            ...theme.authFormOtherPageLinkText,
-                            backgroundColor: "bg.aliceBlue",
-                            padding: "20px",
-                            borderRadius: "4px",
-                        }}
-                    >
-                        Didn't got reset password link?
-                        <Link href="/forgotpassword">
-                            <a
-                                style={{
-                                    ...theme.authFormLink,
-                                    borderBottom: `1px solid ${theme.palette.text.primary}`,
-                                }}
-                            >
-                                Request Another One
-                            </a>
-                        </Link>
-                    </Typography>
-                </Box>
-            </Box>
-            {alert.showAlert && <CustomAlert />}
-        </>
-    );
+              >
+                Login Here
+              </a>
+            </Link>
+          </Typography>
+          <Typography
+            sx={{
+              ...theme.authFormOtherPageLinkText,
+              backgroundColor: "bg.aliceBlue",
+              padding: "20px",
+              borderRadius: "4px",
+            }}
+          >
+            Didn&apos;t got reset password link?
+            <Link href="/forgotpassword">
+              <a
+                style={{
+                  ...theme.authFormLink,
+                  borderBottom: `1px solid ${theme.palette.text.primary}`,
+                }}
+              >
+                Request Another One
+              </a>
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+      {alert.showAlert && <CustomAlert />}
+    </>
+  );
 };
 
 export default ResetPassword;
 
 ResetPassword.getLayout = (page) => {
-    return <>{page}</>;
+  return <>{page}</>;
 };
